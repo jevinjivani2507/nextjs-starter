6faa5aa4542f3ae82b51d9939ce809f392f988dd
jevinjivani2507 <jivani.jevin25@gmail.com>
2025-05-16 23:05:14 +0530

    feat: enable app directory and configure esmExternals in next.config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
+    appDir: true,
+    esmExternals: "loose",
     serverComponentsExternalPackages: ["mongoose"],
   },
 };
